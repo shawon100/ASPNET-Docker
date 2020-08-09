efe3ff108151f305807236632a7c382d36769216
shawon100 <ashadullah.shawon@selise.ch>
2020-08-09 18:52:19 +0600

    Removed Bug

---
+++ aspnetapp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Core_HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is my First MVC Core 2.1 App.";
+            return View();
+        }
+    }
+}
